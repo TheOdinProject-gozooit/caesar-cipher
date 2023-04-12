be222abb7f57e8c2bf504bac7c7010d23ab7819f
gozooit <gozoo.it@protonmail.com>
2023-04-12 09:31:53 +0200

    add shifter and refractor letter? check
    shifter does the shift and automaticaly wraps
    letter? check if the byte correspond to a letter

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -3,11 +3,21 @@ lower_range = (97..122)
 
 def caesar_cipher(str, shift_factor)
   str_bytes = str.bytes
-  str_bytes.map! do |byte|
-    if lower_range.include?(byte) || upper_range.include?(byte)
-      byte += shift_factor
-      # TODO: implement wrapper here
-    end
-  end
+  str_bytes.map! { |byte| shifter(byte, shift_factor) if letter?(byte) }
   str_bytes.pack('c*')
+end
+
+def shifter(byte, shift_factor)
+  shifted_byte = byte + shift_factor
+  if upper_range.include?(byte) && !upper_range.include?(shifted_byte)
+    shifted_byte - 26
+  elsif lower_range.include?(byte) && !lower_range.include?(shifted_byte)
+    shifted_byte - 26
+  else
+    shifted_byte
+  end
+end
+
+def letter?(byte)
+  lower_range.include?(byte) || upper_range.include?(byte)
 end
