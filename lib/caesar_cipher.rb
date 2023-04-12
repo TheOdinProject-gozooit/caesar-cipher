cada942a3868de8b60432bfe4d8f4891aadfb868
gozooit <gozoo.it@protonmail.com>
2023-04-12 09:46:57 +0200

    fix .map! to return byte if not a letter

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,23 +1,27 @@
-upper_range = (65..90)
-lower_range = (97..122)
+UPPER_RANGE = (65..90).freeze
+LOWER_RANGE = (97..122).freeze
 
+# Returns a shifted string accordingly to the shift factor
 def caesar_cipher(str, shift_factor)
   str_bytes = str.bytes
-  str_bytes.map! { |byte| shifter(byte, shift_factor) if letter?(byte) }
+  str_bytes.map! { |byte| letter?(byte) ? shifter(byte, shift_factor) : byte }
   str_bytes.pack('c*')
 end
 
+# Return the shifted byte accordingly to the shift factor, includes the wraps
 def shifter(byte, shift_factor)
   shifted_byte = byte + shift_factor
-  if upper_range.include?(byte) && !upper_range.include?(shifted_byte)
+  if UPPER_RANGE.include?(byte) && !UPPER_RANGE.include?(shifted_byte)
     shifted_byte - 26
-  elsif lower_range.include?(byte) && !lower_range.include?(shifted_byte)
+  elsif LOWER_RANGE.include?(byte) && !LOWER_RANGE.include?(shifted_byte)
     shifted_byte - 26
   else
     shifted_byte
   end
 end
 
+# Return true if the byte correspond to a letter, otherwise return false
 def letter?(byte)
-  lower_range.include?(byte) || upper_range.include?(byte)
+  LOWER_RANGE.include?(byte) || UPPER_RANGE.include?(byte)
 end
+
